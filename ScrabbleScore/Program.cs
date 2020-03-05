5d20f5ad0752a1bee2991372c7eb18a41d21ba3d
Krista Rutz and Steven Fleming <krista+steven@>
2020-03-05 09:09:46 -0800

    when main program runs, user is prompted to enter a word. the program returns a message repeating the word and assigning it a score value, currently set to zero

--- ScrabbleScore/Program.cs
+++ ScrabbleScore/Program.cs
@@ -6,7 +6,10 @@ namespace ScrabbleScore
   {
     public static void Main()
     {
-
+      Console.WriteLine("Enter your word to calculate it's Scrabble score.");
+      string userInput = Console.ReadLine();
+      int score = 0;
+      Console.WriteLine($"{userInput} is worth {score} points.");
     }
   }
 }
